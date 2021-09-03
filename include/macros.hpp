0c4ff837dd3a1e676234f668984ef90540d4eea8
alexdevteam <alexpro820@gmail.com>
2021-09-03 17:05:23 +0200

    Assert on unreachable if PLIB_DEBUG is defined

--- include/macros.hpp
+++ include/macros.hpp
@@ -4,10 +4,15 @@
 #define __has_builtin(_) 0
 #endif
 
-#if __has_builtin(__builtin_unreachable)
-#    define PLIB_UNREACHABLE() __builtin_unreachable()
-#elif MSVC
-#    define PLIB_UNREACHABLE() __assume(0)
+#if PLIB_DEBUG
+#   include <cassert>
+#   define PLIB_UNREACHABLE() assert(false && "Entered unreachable state")
 #else
-#    define PLIB_UNREACHABLE()
+#   if __has_builtin(__builtin_unreachable)
+#       define PLIB_UNREACHABLE() __builtin_unreachable()
+#   elif MSVC
+#       define PLIB_UNREACHABLE() __assume(0)
+#   else
+#       define PLIB_UNREACHABLE()
+#   endif
 #endif
